64a7cb0ebae4c57fc8ea90c9c1b9cd992d6d5497
Wanlop <6811860007@mut.ac.th>
2025-11-23 17:02:33 +0700

    การทดลองครั้งที่ 2

---
+++ LAB02/TEST01/TEST01.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello LAB2!" << std::endl;
+    std::cout << "Name: Sutit Ongart" << std::endl;
+    std::cout << "Course: MIIA0106" << std::endl;
+    std::cout << "Email:sutit.o@maori-softech.com" << std::endl;
+    return 0;
+}
+ฆ
