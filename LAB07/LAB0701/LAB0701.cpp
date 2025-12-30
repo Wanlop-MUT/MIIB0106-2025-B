0c882393744ece12e1828d012b1616af7dab3795
Wanlop <6811860007@mut.ac.th>
2025-12-30 14:53:54 +0700

    LAB07 Pointer

---
+++ LAB07/LAB0701/LAB0701.cpp
@@ -0,0 +1,16 @@
+﻿// TODO: แสดงค่า score, address และ pointer
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score = 50;
+    int* ptr = &score;
+
+    // TODO
+	cout << "Value of score = " << score << endl;
+	cout << "Address of score = " << &score << endl;
+	cout << "Value of pointer = " << ptr << endl;
+	cout << "Value pointed to by pointer = " << *ptr << endl;
+
+    return 0;
+}
